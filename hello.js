a849ec24a8c51b052d7a6fbda89bb88be59afd13
GANESH KASHYAP <ganesh.ucf@gmail.com>
2025-04-01 10:20:36 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+
+const add = (a,b)=>{
+    return a+b;
+}
+ 
+// module.exports={add}
+// function add(a+b){
+//     return a+b;
+// }
+// console.log(add(6,7));  
+
---
+++ index.js
@@ -0,0 +1,5 @@
+const add= from"./hello.js";
+
+
+console.log(add);
+console.log(add(3,7));
